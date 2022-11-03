eaf9afd05f527dee52fcb158f76ccdbd49912641
Mateusz Pruchnik <mateuszpruchnik96@wp.pl>
2022-11-03 19:31:54 +0100

    Fetch after login works, have to add "fetch again" after refresh

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,32 +1,35 @@
 import axios from "axios";
-// import { useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 // import { signIn } from "actions";
 
 // const dispatch = useDispatch();
-let token = null;
-if (localStorage.easyValuationToken) {
-  token = localStorage.easyValuationToken;
-  console.log(token);
-} else {
-  token = null;
-}
+
+export const getToken = () =>
+  localStorage.getItem("easyValuationToken")
+    ? localStorage.getItem("easyValuationToken")
+    : null;
+
+export const getAuthorizationHeader = () => `Bearer ${getToken()}`;
 
 const instance = axios.create({
   baseURL: "http://localhost:8080/",
   withCredentials: true,
 });
-instance.defaults.headers.common[`Authorization`] = `Bearer ${token}`;
+// instance.defaults.headers.common[`Authorization`] = `Bearer ${token}`;
 instance.defaults.headers.common["Content-Type"] = "application/json";
 
 instance.interceptors.request.use(async function (request) {
   console.log("Request interceptor");
+
+  request.headers.Authorization = getAuthorizationHeader();
+
   return request;
 });
 
 instance.interceptors.response.use(
   async function (response) {
-    console.log("Response interceptor");
-    console.log(response);
+    // console.log("Response interceptor");
+    // console.log(response);
 
     return response;
   },
@@ -34,7 +37,7 @@ instance.interceptors.response.use(
     console.log("Response error interceptor", error);
     if (error.response.status === 401) {
       const { config, message } = error.response;
-      console.log(config);
+      // console.log(config);
 
       const refreshResponse = await axios
         .post(
@@ -42,7 +45,7 @@ instance.interceptors.response.use(
           localStorage.easyValuationRefreshToken,
           {
             headers: {
-              Authorization: `Bearer ${token}`,
+              Authorization: `${getAuthorizationHeader()}`,
               "Content-Type": "text/plain",
             },
           }
@@ -55,18 +58,18 @@ instance.interceptors.response.use(
       if (refreshResponse.status != 200) {
         throw new Error(`Refresh response status: ${refreshResponse.status}`);
       } else {
-        console.log(localStorage.easyValuationToken);
         localStorage.setItem(
           "easyValuationToken",
           refreshResponse.data.easyValuationToken
         );
-        console.log(refreshResponse.data.easyValuationToken);
-        console.log(localStorage.easyValuationToken);
 
         localStorage.setItem(
           "easyValuationRefreshToken",
           refreshResponse.data.easyValuationRefreshToken
         );
+
+        // console.log(refreshResponse.data.easyValuationToken);
+        // console.log(localStorage.easyValuationToken);
 
         return () => axios(config);
 
